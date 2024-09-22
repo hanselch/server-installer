fa529856cae17f934ea692cc254a4368c72c24f0
Hansel Chavez <118041557+hanselch@users.noreply.github.com>
2024-09-22 02:45:30 -0600

    Update validatemodules.py
    v0.3.2-a: (#1) added specific version control on packages

--- preProgram/validatemodules.py
+++ preProgram/validatemodules.py
@@ -1,18 +1,18 @@
-needed_modules = [
-    "tk",
-    "gdown",
-    "pyyaml",
-]
+needed_modules = {
+    "tk": ">=0.1.0",
+    "gdown": ">=5.2.0",
+    "pyyaml": ">=6.0.0",
+}
 
 import subprocess, sys
 
-def install_pip(package):
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+def install_pip(package, version):
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', f"{package}{version}"])
 
 def get_requirements():
     print(f"Double checking if your system have the required files...")
     spec = subprocess.check_output([sys.executable, "-m", "pip", "freeze"])
     installed_packages = [r.decode().split('==')[0] for r in spec.split()]
-    for package in needed_modules:
+    for (package, version) in needed_modules.items():
         if package not in installed_packages:
-            install_pip(package)
+            install_pip(package, version)
